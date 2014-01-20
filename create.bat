eb4df821804dc1170f6369edf10c1612906ac3de
EasyListIsrael <easylistisrael@gmail.com>
2014-01-20 17:40:09 +0200

    rename list file

--- create.bat
+++ create.bat
@@ -1,8 +1,8 @@
 @echo off
-python tools\addChecksum.py < IsraelList.txt > IsraelList2.txt
+python tools\addChecksum.py < EasyListHebrew.txt > IsraelList2.txt
 python tools\validateChecksum.py < IsraelList2.txt
 IF ERRORLEVEL 1 GOTO errorHandling
-move /y IsraelList2.txt IsraelList.txt
+move /y IsraelList2.txt EasyListHebrew.txt
 exit
 :errorHandling
 echo Somthing went wrong
