7fa1317385d15f4973a935e838f1868b7eff7530
IsraeliUser <anon9137@gmail.com>
2020-02-17 20:16:16 +0200

    create-OSX.sh https://github.com/easylist/EasyListHebrew/pull/336

---
+++ create-OSX.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+tmpList=$(mktemp)
+python2 tools/addChecksum.py < EasyListHebrew.txt > $tmpList
+python2 tools/validateChecksum.py < $tmpList
+if [ $? == 0 ] ; then
+  mv -f $tmpList EasyListHebrew.txt
+else
+  echo 'something went wrong'
+fi
