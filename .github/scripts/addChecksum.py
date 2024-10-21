540d2e8433e95a9487914685acc97b72bea09c67
Hebrew-uBO-User <103670534+Hebrew-uBO-User@users.noreply.github.com>
2024-10-21 03:52:02 +0300

    fix https://github.com/easylist/EasyListHebrew/pull/419

--- .github/scripts/addChecksum.py
+++ .github/scripts/addChecksum.py
@@ -27,7 +27,6 @@
 #                                                                           #
 # Note: your subscription file should be saved in UTF-8 encoding, otherwise #
 # the operation will fail.                                                  #
-#                                                                           #
 #############################################################################
 
 import io, sys, re, hashlib, base64, datetime
@@ -35,36 +34,38 @@ import io, sys, re, hashlib, base64, datetime
 checksumRegexp = re.compile(r"^\s*!\s*checksum[\s\-:]+([\w\+\/=]+).*\n", re.I | re.M)
 dateRegexp = re.compile(r"^\s*!\s*Last modified[\s\-:]+([\w\+\/=]+).*\n", re.I | re.M)
 
-
 def addChecksum(data):
+    # Update the last modified date
     data = re.sub(
         dateRegexp,
-        "! Last modified: %s"
-        % datetime.datetime.utcnow().strftime("%d %b %Y %H:%M UTC\n"),
+        "! Last modified: %s" % datetime.datetime.utcnow().strftime("%d %b %Y %H:%M UTC\n"),
         data,
     )
     checksum = calculateChecksum(data)
+    # Remove existing checksums
     data = re.sub(checksumRegexp, "", data)
+    # Add the new checksum
     data = re.sub(r"(\r?\n)", r"\1! Checksum: %s\1" % checksum, data, 1)
     return data
 
-
 def calculateChecksum(data):
     md5 = hashlib.md5()
+    # Normalize the data for checksum calculation
     md5.update(normalize(data).encode("utf-8"))
     return base64.b64encode(md5.digest()).rstrip(b"=").decode()
 
-
 def normalize(data):
-    data = re.sub(r"\r", "", data)
-    data = re.sub(r"\n+", "\n", data)
-    data = re.sub(checksumRegexp, "", data)
-    return data
-
+    # Split the data into lines
+    lines = data.splitlines()
+    
+    # Filter out lines containing the checksum
+    filtered_lines = [line for line in lines if not re.match(r"^\s*!?\s*checksum\s*:", line, re.I)]
+    
+    # Join remaining lines for checksum calculation
+    return "\n".join(filtered_lines)
 
 # Coded by EasyList Hebrew Team
 # Licence: https://easylist.to/pages/licence.html
-
 
 def sort_file(data):
     lines = data.split("\n") + ["\n"]
@@ -147,10 +148,9 @@ def sort_file(data):
                 lst.append(line + "\n")
     return res.rstrip()
 
-
 if __name__ == "__main__":
-    with open(sys.argv[1], "r") as f:
+    with open(sys.argv[1], "r", encoding='utf-8') as f:
         data = f.read()
         data = addChecksum(sort_file(data))
-    with io.open(sys.argv[2], "w", newline='\n') as f:
+    with io.open(sys.argv[2], "w", newline='\n', encoding='utf-8') as f:
         f.write(data)
